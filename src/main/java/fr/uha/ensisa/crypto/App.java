a4d1630a8910ef817048f1bd3f5313d7314f93a7
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2022-12-22 14:22:01 +0100

    created project using maven quickstart

---
+++ src/main/java/fr/uha/ensisa/crypto/App.java
@@ -0,0 +1,13 @@
+package fr.uha.ensisa.crypto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
