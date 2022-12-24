e15969a3477df4238da303db48deb83ac66d9799
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2022-12-24 16:28:10 +0100

    refactored create button -> create event button

--- src/main/java/fr/uha/ensisa/crypto/ui/topbar/TopBarPanel.java
+++ src/main/java/fr/uha/ensisa/crypto/ui/topbar/TopBarPanel.java
@@ -11,31 +11,31 @@ import fr.uha.ensisa.crypto.ui.MainWindowController;
 
 public class TopBarPanel extends JPanel implements ActionListener {
 
-    private static final String CREATE_BUTTON_TEXT = "NEW EVENT";
-    private static final String CREATE_BUTTON_TOOLTIP = "Click here to create a new event";
+    private static final String CREATE_EVENT_BUTTON_TEXT = "NEW EVENT";
+    private static final String CREATE_EVENT_BUTTON_TOOLTIP = "Click here to create a new event";
 
     private MainWindow mainWindow;
     private MainWindowController controller;
-    private JButton createButton;
+    private JButton createEventButton;
 
     public TopBarPanel(MainWindow mainWindow) {
         this.mainWindow = mainWindow;
         this.controller = this.mainWindow.getController();
 
         // create button
-        this.createButton = new JButton(CREATE_BUTTON_TEXT);
-        this.createButton.setToolTipText(CREATE_BUTTON_TOOLTIP);
+        this.createEventButton = new JButton(CREATE_EVENT_BUTTON_TEXT);
+        this.createEventButton.setToolTipText(CREATE_EVENT_BUTTON_TOOLTIP);
 
         // handle click
-        this.createButton.addActionListener(this);
+        this.createEventButton.addActionListener(this);
 
         // add button(s)
-        this.add(this.createButton);
+        this.add(this.createEventButton);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource().equals(this.createButton))
+        if (e.getSource().equals(this.createEventButton))
             this.createEventPopup();
     }
 
