3d97e333346f837adbbc4a0eb39bd73a9eb90e19
Simon Kempf <simon25042001@gmail.com>
2022-12-26 19:21:27 +0100

    ajout

--- src/main/java/fr/uha/ensisa/crypto/ui/topbar/CreateCalendarPopup.java
+++ src/main/java/fr/uha/ensisa/crypto/ui/topbar/CreateCalendarPopup.java
@@ -24,6 +24,7 @@ public class CreateCalendarPopup extends JDialog implements ActionListener {
     private static final String POPUP_TITLE = "New Calendar";
     private static final String CALENDAR_LABEL_TEXT = "CALENDAR";
     private static final String PASS_LABEL_TEXT = "PASSWORD";
+    private static final String PASS_CONFIRM_LABEL_TEXT = "CONFIRM PASSWORD";
     private static final String ALGO_LABEL_TEXT = "ALGORITHM";
     private static final String CREATE_BUTTON_TEXT = "create";
 
@@ -31,6 +32,7 @@ public class CreateCalendarPopup extends JDialog implements ActionListener {
     private static final int POPUP_HEIGHT = 100;
     private static final int CALENDAR_TEXT_FIELD_LENGTH = 10;
     private static final int PASS_TEXT_FIELD_LENGTH = 10;
+    private static final int PASS_CONFIRM_TEXT_FIELD_LENGTH = 10;
 
     private MainWindow mainWindow;
     private JPanel mainPanel;
@@ -40,6 +42,8 @@ public class CreateCalendarPopup extends JDialog implements ActionListener {
     private JTextField calendarTextField;
     private JLabel passLabel;
     private JTextField passTextField;
+    private JLabel passConfirmLabel;
+    private JTextField passConfirmTextField;
     private JLabel algoLabel;
     private JComboBox<String> algoListField;
     private JButton createButton;
@@ -51,7 +55,7 @@ public class CreateCalendarPopup extends JDialog implements ActionListener {
 
         // create main panel to store everything in
         this.mainPanel = new JPanel();
-        this.layout = new GridLayout(4, 2);
+        this.layout = new GridLayout(5, 2);
         this.mainPanel.setPreferredSize(new Dimension(POPUP_WIDTH, POPUP_HEIGHT));
         this.setPreferredSize(this.mainPanel.getPreferredSize());
         this.setMinimumSize(this.mainPanel.getPreferredSize());
@@ -62,24 +66,28 @@ public class CreateCalendarPopup extends JDialog implements ActionListener {
         // label
         this.calendarLabel = new JLabel(CALENDAR_LABEL_TEXT);
         this.passLabel = new JLabel(PASS_LABEL_TEXT);
+        this.passConfirmLabel = new JLabel(PASS_CONFIRM_LABEL_TEXT);
         this.algoLabel = new JLabel(ALGO_LABEL_TEXT);
 
         // textfield
         this.calendarTextField = new JTextField(CALENDAR_TEXT_FIELD_LENGTH);
         this.passTextField = new JPasswordField(PASS_TEXT_FIELD_LENGTH);
+        this.passConfirmTextField = new JPasswordField(PASS_CONFIRM_TEXT_FIELD_LENGTH);
 
         // button
         this.createButton = new JButton(CREATE_BUTTON_TEXT);
         this.createButton.addActionListener(this);
 
         // list
-        this.algoListField = new JComboBox<String>(new String[]{"ALGO 1","ALGO 2"});
+        this.algoListField = new JComboBox<String>(new String[]{"NONE", "ALGO 1", "ALGO 2"});
         
         // add components
         this.mainPanel.add(this.calendarLabel);
         this.mainPanel.add(this.calendarTextField);
         this.mainPanel.add(this.passLabel);
         this.mainPanel.add(this.passTextField);
+        this.mainPanel.add(this.passConfirmLabel);
+        this.mainPanel.add(this.passConfirmTextField);
         this.mainPanel.add(this.algoLabel);
         this.mainPanel.add(this.algoListField);
 
@@ -96,20 +104,31 @@ public class CreateCalendarPopup extends JDialog implements ActionListener {
 
     private void createCalendar() {
         String calendarName = this.calendarTextField.getText();
-
-        // required field : calendar title
-        if (calendarName.isBlank()) {
-            this.showErrorPopup("Please specify a name for the calendar.");
-        } else {
-            try {
-                MainWindowController controller = this.mainWindow.getController();
-                controller.createCalendar(calendarName);
-                this.dispose();
-            } catch (IOException | Error e) {
-                // if an error occured, display a dialog indicating what is wrong
-                this.showErrorPopup("Creation failed !");
-                e.printStackTrace();
-            }
+        String password = this.passTextField.getText();
+        String passwordConfirm = this.passConfirmTextField.getText();
+        if(password.contentEquals(passwordConfirm)) {
+	        int indexAlgo = this.algoListField.getSelectedIndex();
+	        if(password.isBlank() && indexAlgo != 0) {
+	        	this.showErrorPopup("Please specify a password.");
+	        } else {
+		        // required field : calendar title
+		        if (calendarName.isBlank()) {
+		            this.showErrorPopup("Please specify a name for the calendar.");
+		        } else {
+		            try {
+		                MainWindowController controller = this.mainWindow.getController();
+		                controller.createCalendar(calendarName);
+		                this.dispose();
+		            } catch (IOException | Error e) {
+		                // if an error occured, display a dialog indicating what is wrong
+		                this.showErrorPopup("Creation failed !");
+		                e.printStackTrace();
+		            }
+		        }
+	        }
+        }
+        else {
+        	this.showErrorPopup("Password and confirm must be equal.");
         }
     }
 
