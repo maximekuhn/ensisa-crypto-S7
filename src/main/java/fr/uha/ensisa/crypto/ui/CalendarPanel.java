d6c72bff8ff506927c79685c087c8853d4aba270
BISEL Jean <jean@langmatt.com>
2022-12-27 16:30:31 +0100

    duration support + stylage

--- src/main/java/fr/uha/ensisa/crypto/ui/CalendarPanel.java
+++ src/main/java/fr/uha/ensisa/crypto/ui/CalendarPanel.java
@@ -8,6 +8,7 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -28,8 +29,8 @@ public class CalendarPanel extends JScrollPane {
         this.setBorder(BorderFactory.createLineBorder(new Color(50, 50, 50)));
         selectedDate = new Date();
         grid = new JPanel();
-        grid.setLayout(new GridLayout(24, 7, 0, 0));
-        grid.setPreferredSize(new Dimension(this.getWidth(), 24 * 50));
+        grid.setLayout(new GridLayout(48, 7, 0, 0));
+        grid.setPreferredSize(new Dimension(this.getWidth(), 24 * 90));
         refreshGrid();
         this.setViewportView(grid);
         grid.setBackground(new Color(50, 50, 50));
@@ -48,23 +49,48 @@ public class CalendarPanel extends JScrollPane {
 
 
 
-        for (int i=0; i<168; i++){
+        for (int i=0; i<336; i++) {
             int day = i%7;
-            int hour = i/7;
-            Date current = addDaysHours(monday,day,hour);
+            int minute = 30*(i/7);
+            Date current = addDaysMinutes(monday,day,minute);
+            JPanel panel = new JPanel();
+            JLabel label = new JLabel(
+                    "<html><center>"+
+                            current.getDate()+"/"+(current.getMonth()+1)+"/"+(1900+current.getYear())+
+                            "<br>"+current.getHours()+":"+current.getMinutes()+"<br>"+
+                            "</center></html>"
+            );
+            if(day>=5)
+                panel.setBackground(new Color(50,50,50));
+            else
+                panel.setBackground(new Color(40,40,40));
+
+            label.setForeground(new Color(80,80,80));
+
+            panel.add(label);
+            panel.setBorder(BorderFactory.createMatteBorder(0, 1, 0, 1, new Color(60, 60, 60)));
+
+//            panel.setBorder(BorderFactory.createLineBorder(new Color(100, 100, 110)));
+            grid.add(panel);
+        }
+
+
+        for (int i=0; i<336; i++) {
+            int day = i%7;
+            int minute = 30*(i/7);
+            Date current = addDaysMinutes(monday,day,minute);
             ArrayList<Event> events = new ArrayList<Event>();
             for (Calendar calendar:calendars) {
                 EventTable eventTable = calendar.getEventTable();
                 Event event = eventTable.search(current);
                 if(event != null) {
                     events.add(event);
-                    System.out.println("Found event at "+current.toString());
                 }
-
             }
-            JPanel panel = new JPanel();
+            JPanel panel = (JPanel) grid.getComponent(i);
             StringBuilder tooltip = new StringBuilder("<html>");
             if (events.size()>0) {
+                panel.removeAll();
                 for (Event e: events) {
                     panel.add(new JLabel(e.getEvent()));
                     tooltip.append("<center><strong>").append(e.getEvent()).append("</center></strong><br>");
@@ -72,29 +98,34 @@ public class CalendarPanel extends JScrollPane {
                     tooltip.append("Lieu: ").append(e.getLocation()).append("<br>");
                     tooltip.append("Durée: ").append(e.getDuration()).append("<br>");
 
-
                 }
                 tooltip.append("</html>");
+                panel.setBorder(BorderFactory.createMatteBorder(0,1,0,1,new Color(60, 60, 60)));
+                panel.setBackground(new Color(200, 100, 150));
                 panel.setToolTipText(tooltip.toString());
-                panel.setBorder(BorderFactory.createLineBorder(new Color(100, 100, 110)));
-                panel.setBackground(new Color(200,100,150));
+                for (Event e: events) {
+                    for (int j = 0; j < e.getDuration()*2; j++) {
+                        JPanel panel2 = (JPanel) grid.getComponent(j*7+i);
+                        if(j==0 && e.getDuration()>1) {
+                            panel2.setBorder(BorderFactory.createMatteBorder(1, 1, 0, 1, new Color(60, 60, 60)));
+                        }
+                        else if(j>0) {
+                            panel2.removeAll();
+                            if (j == (e.getDuration() * 2 - 1))
+                                panel2.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, new Color(60, 60, 60)));
+                            else
+                                panel2.setBorder(BorderFactory.createMatteBorder(0, 1, 0, 1, new Color(60, 60, 60)));
+
+
+                            panel2.setBackground(new Color(200, 100, 150));
+                            panel2.setToolTipText(tooltip.toString());
+                        }
+                    }
+
+                }
 
             }
-            else {
-                JLabel label = new JLabel(
-                        "<html><center>"+
-                                current.getDate()+"/"+(current.getMonth()+1)+"/"+(1900+current.getYear())+
-                                "<br>"+current.getHours()+":00<br>"+
-                                "</center></html>"
-                );
-                panel.setBackground(new Color(50,50,50));
-                label.setForeground(new Color(80,80,80));
 
-                panel.add(label);
-
-                panel.setBorder(BorderFactory.createLineBorder(new Color(100, 100, 110)));
-            }
-            grid.add(panel);
         }
         revalidate();
     }
@@ -104,10 +135,10 @@ public class CalendarPanel extends JScrollPane {
         refreshGrid();
     }
 
-    private Date addDaysHours(Date current,int days,int hours) {
+    private Date addDaysMinutes(Date current,int days,int minutes) {
         java.util.Calendar calendar = java.util.Calendar.getInstance();
         calendar.setTime(current);
-        calendar.add(java.util.Calendar.HOUR_OF_DAY, hours);
+        calendar.add(java.util.Calendar.MINUTE, minutes);
         calendar.add(java.util.Calendar.DATE, days);
 
         return calendar.getTime();
